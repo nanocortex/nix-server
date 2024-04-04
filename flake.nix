8c65338c159f21892afc4d59ba5e5442fe35d561
nb.dnio6 <nb.dnio6@aleeas.com>
2024-04-04 23:00:33 +0200

    feat: modular

--- flake.nix
+++ flake.nix
@@ -14,24 +14,61 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs: {
-    nixosConfigurations = {
-	nixos = nixpkgs.lib.nixosSystem {
-	      system = "aarch64-linux";
-	      modules = [
-		./configuration.nix # Import the previous configuration.nix
-   home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            # TODO replace ryan with your own username
-            home-manager.users.user = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
-          }
-	      ];
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs ["aarch64-linux" "x86_64-linux"];
+    forEachPkgs = f: forEachSystem (sys: f nixpkgs.legacyPackages.${sys});
+  mkNixos = host: system:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {inherit (self) inputs outputs;};
+        modules = [
+          ./hosts/${host}
+        ];
       };
+
+    mkHome = host: system:
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        extraSpecialArgs = {inherit (self) inputs outputs;};
+        modules = [
+          ./home/edgar/${host}.nix
+        ];
+      };
+
+in
+{
+
+    packages = forEachPkgs (pkgs: import ./pkgs {inherit pkgs;});
+
+    nixosConfigurations = {
+      cronos = mkNixos "cronos" "aarch64-linux";
+ #    hestia = mkNixos "hestia" "x86_64-linux";
+ #    deimos = mkNixos "deimos" "x86_64-linux";
     };
+
+    homeConfigurations = {
+     "dave@cronos" = mkHome "cronos" "aarch64-linux";
+ #    "edgar@hestia" = mkHome "hestia" "x86_64-linux";
+ #    "edgar@deimos" = mkHome "deimos" "x86_64-linux";
+    };
+
+#    nixosConfigurations = {
+#	nixos = nixpkgs.lib.nixosSystem {
+#	      system = "aarch64-linux";
+#	      modules = [
+#		./configuration.nix # Import the previous configuration.nix
+#  home-manager.nixosModules.home-manager
+#         {
+#           home-manager.useGlobalPkgs = true;
+#           home-manager.useUserPackages = true;
+
+#           # TODO replace ryan with your own username
+#           home-manager.users.user = import ./home.nix;
+
+#           # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+#         }
+#	      ];
+#      };
+#    };
   };
 }

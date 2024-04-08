82c0bd8cbba3a8f4d3efab35a4f3cffadc9d6090
nb.dnio6 <nb.dnio6@aleeas.com>
2024-04-08 22:02:36 +0200

    fix install

--- scripts/install.sh
+++ scripts/install.sh
@@ -58,6 +58,6 @@ rm -rf /mnt/etc/nixos/*
 ln -s /mnt/etc/nixos /mnt/home/user/dotfiles
 
 cp /tmp/ssh_host_ed25519_key /mnt/etc/ssh
-cp /tm/ssh_host_ed25519_key.pub /mnt/etc/ssh
+cp /tmp/ssh_host_ed25519_key.pub /mnt/etc/ssh
 
 echo "Installation complete. Before rebooting copy ssh keys!!!"

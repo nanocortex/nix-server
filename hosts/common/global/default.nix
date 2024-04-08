66c7b6781847f338e000ac3c659f999789a94d24
nb.dnio6 <nb.dnio6@aleeas.com>
2024-04-08 23:31:25 +0200

    try to use initrd for ssh

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -8,13 +8,28 @@
 #     ./nixpkgs.nix
 #     ./ntfs.nix
 #     ./oomd.nix
-     ./openssh.nix
-     ./sops.nix
+      ./openssh.nix
+      ./sops.nix
 #     ./systemd-boot.nix
-     ./tailscale.nix
-    ./users.nix
+      ./tailscale.nix
+      ./users.nix
   ];
 
 
   time.timeZone = "Europe/Warsaw";
+
+  boot.kernelParams = [ "ip=dhcp" ];
+  boot.initrd = {
+    availableKernelModules = [ "r8169" ];
+    systemd.users.root.shell = "/bin/cryptsetup-askpass";
+    network = {
+      enable = true;
+      ssh = {
+        enable = true;
+        port = 22;
+        authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIED8axnZzZk5P39CEbMeJZy42o/4T0iHgr1pefNDsnU5 ex0cortex@pm.me" ];
+        hostKeys = [ "/etc/secrets/initrd/ssh_host_rsa_key" ];
+      };
+    };
+  };
 }

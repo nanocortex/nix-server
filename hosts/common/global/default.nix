a1fa4cb2fed4d85ca6361b2b537631883a5c23b7
nb.dnio6 <nb.dnio6@aleeas.com>
2024-04-09 17:29:51 +0200

    fix

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -18,19 +18,19 @@
 
   time.timeZone = "Europe/Warsaw";
 
-  # boot.kernelParams = [ "ip=dhcp" ];
-  # boot.initrd = {
-  #   verbose = true;
-  #   availableKernelModules = [ "r8169" "virtio-pci" ];
-  #   systemd.users.root.shell = "/bin/systemd-tty-ask-password-agent";
-  #   network = {
-  #     enable = true;
-  #     ssh = {
-  #       enable = true;
-  #       port = 22;
-  #       authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIED8axnZzZk5P39CEbMeJZy42o/4T0iHgr1pefNDsnU5 ex0cortex@pm.me" ];
-  #       hostKeys = [ "/etc/secrets/initrd/ssh_host_rsa_key" ];
-  #     };
-  #   };
-  # };
+  boot.kernelParams = [ "ip=dhcp" ];
+  boot.initrd = {
+    verbose = true;
+    availableKernelModules = [ "virtio-pci" ];
+    systemd.users.root.shell = "/bin/systemd-tty-ask-password-agent";
+    network = {
+      enable = true;
+      ssh = {
+        enable = true;
+        port = 22;
+        authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIED8axnZzZk5P39CEbMeJZy42o/4T0iHgr1pefNDsnU5 ex0cortex@pm.me" ];
+        hostKeys = [ "/etc/secrets/initrd/ssh_host_rsa_key" ];
+      };
+    };
+  };
 }
